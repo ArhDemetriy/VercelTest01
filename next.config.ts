fc75249e3da651f6749f339d65ef832a318d57b9
Dmitriy Nazimov <ArhDemetriy@GMail.com>
2025-02-04 23:42:34 +0800

    dev

--- next.config.ts
+++ next.config.ts
@@ -5,15 +5,6 @@ const nextConfig: NextConfig = {
     productionBrowserSourceMaps: true, // TODO удалить перед релизом
     staticPageGenerationTimeout: 1000,
     output: 'standalone',
-    webpack(
-        /** @type {import('webpack').Configuration} */
-        config,
-    ) {
-        if (config.externals == null) config.externals = []
-        if (!Array.isArray(config.externals)) config.externals = [config.externals]
-
-        return config
-    },
 }
 
 export default nextConfig
